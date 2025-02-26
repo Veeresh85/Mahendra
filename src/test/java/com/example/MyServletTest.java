3e599ba06fa845ba7188a8d91269c50ae8d5ce26
Veeresh85 <veeresh.settyb@gmail.com>
2025-02-26 18:50:11 +0100

    Create MyServletTest.java

---
+++ src/test/java/com/example/MyServletTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyServletTest {
+
+    @Test
+    public void dummyTest() {
+        assertTrue(true); // A simple test to verify JUnit is working.
+    }
+}
